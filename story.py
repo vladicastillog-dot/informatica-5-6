babecd50fa3a416301c2dc673baf1c6c028b973c
vladicastillog-dot <vladicastillog@gmail.com>
2026-08-13 15:39:37 +0000

    blue

--- story.py
+++ story.py
@@ -14,17 +14,23 @@ def main():
     # print("Hello", end=" ")
     # print(planet)
 
-    name = input("What is your name? ")
-    color = input("Whats your favorite colour? ")
-    andjetive =input("Tell me an adjetive: ")
-    goal = input("Tell me a goal: ")
+    name = input("What is your name? ").strip().title()
+    color = input("Whats your favorite colour? ").strip().lower()
+    andjetive =input("Tell me an adjetive: ").strip().lower()
+    goal = input("Tell me a goal: ").strip().lower()
 
-    print(f"Hello, {name}!", end="\n\n")
+    print(f"Hello, {name}!")
 
     print("This is your story:")
 
-    print(f"At dawn the sky turned {color}, and the air felt {andjetive}")
-    print(f" I decided today I will finally {goal}")
+    print(f"At dawn the sky turned {color}, and the air felt {andjetive}, I decided today I will finally {goal}")
+
+    print(f"At dawn the sky turned {color}, and the air felt {andjetive}, I decided today I will finally {goal}".upper())
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
