f70bd9c0ec69ec642d1b687e9c04bb7bcb88fc2b
vladicastillog-dot <vladicastillog@gmail.com>
2026-08-18 20:19:28 +0000

    currency

---
+++ currency.py
@@ -0,0 +1,18 @@
+def main():
+    pesos = float(input("What do you have left in pesos?: "))
+    soles = float(input("What do you have left in soles?: "))
+    reais = float(input("What do you have left in reais?: "))
+
+    colombia = pesos/3,100.27
+    peru = soles/3.37
+    brazil = reais/5.21
+
+    USA = c
+
+
+
+
+
+
+    if __name__== "__main__":
+        main()
--- rectangle.py
+++ rectangle.py
@@ -1,7 +1,19 @@
 def main():
-    w = int(input("Width: "))
+    l = 5
+    w = int(input("how many Width do you want?: "))
     print("O" * w)
     print("O" * w)
+    print("O" * w)
+    print("O" * w)
+    print("O" * w)
+
+    p = (2 * l) + (2 * w)
+    print ("Perimeter:", p)
+    a = (w * l)
+    print("Area:", a)
+    d = (l**2 + w**2)**(1/2)
+    print("Diagonal:", d)
+     
 
 if __name__== "__main__":
         main()
