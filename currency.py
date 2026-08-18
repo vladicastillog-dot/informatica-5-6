4661db1da68392a6336c1290c150c9e4ac8bf8d9
vladicastillog-dot <vladicastillog@gmail.com>
2026-08-18 21:52:17 +0000

    currency

--- currency.py
+++ currency.py
@@ -3,16 +3,21 @@ def main():
     soles = float(input("What do you have left in soles?: "))
     reais = float(input("What do you have left in reais?: "))
 
-    colombia = pesos/3,100.27
+    colombia = pesos/3100.27
     peru = soles/3.37
     brazil = reais/5.21
 
-    USA = c
+    Usa = colombia + peru + brazil
+    Mex = Usa * 17.07
+    Usa = round(Usa,2)
+    Mex = round(Mex,2)
+    print(f"Usa{Usa}" )
+    print (f"Mex{Mex}" )
 
 
 
 
 
 
-    if __name__== "__main__":
+if __name__== "__main__":
         main()
