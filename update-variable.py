e778383d91e7868f4edf1a6de4a724377b40c6a9
vladicastillog-dot <vladicastillog@gmail.com>
2026-08-19 18:00:12 +0000

    update

---
+++ update-variable.py
@@ -0,0 +1,36 @@
+def main():
+    # Augmented Assignment Operator
+    score = 10
+    score += 10
+
+    #Subtraction Assignment Opeator
+    # x = x-y
+    # x-=y
+    score=100
+    score-=15
+
+    #Multiplication Assignment OPERATOR
+    balance = 10
+    balance *=4
+
+    #Division Assignment Operator
+    x = 10
+    x /= 5
+    print (x+67)
+
+    #Modulus Assignment Operator
+
+    a = 10
+    b = 4
+    a%= b
+    print(a)
+
+
+
+
+
+
+
+
+if __name__== "__main__":
+    main()
