20ad4e73db9032e42fe2104da70626b8bdd0f215
vladicastillog-dot <vladicastillog@gmail.com>
2026-08-24 14:56:35 +0000

    password

---
+++ password.py
@@ -0,0 +1,16 @@
+def main():
+        password= "Maleta09"
+
+        pwd = input("introduce password")
+        if pwd == password:
+            print("Correct password")
+
+        print("we finished")
+
+
+
+
+
+
+if __name__== "__main__":
+        main()
