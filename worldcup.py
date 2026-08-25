37c6efd7e5dbbf8eb49771439d88316723119f5b
vladicastillog-dot <vladicastillog@gmail.com>
2026-08-25 19:55:25 +0000

    worldcup

---
+++ worldcup.py
@@ -0,0 +1,17 @@
+def main ():
+    spain = int(input("Spain goals: "))
+    argentina = int(input("Argentina goals: "))
+
+    if spain > argentina:
+        print("Spain is the winner!!!!")
+    elif argentina > spain:
+        print("Argentina is the winner!!")
+    else:
+        print("Its a tie.")
+
+    print("gg")
+
+
+
+if __name__== "__main__":
+    main()
