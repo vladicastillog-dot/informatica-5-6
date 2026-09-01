88cc10fbaca986e56818ae20a168147b0164ce13
vladicastillog-dot <vladicastillog@gmail.com>
2026-09-01 20:13:36 +0000

    heads

---
+++ coinflip.py
@@ -0,0 +1,25 @@
+import random
+
+def main():
+        choose = int(input("heads(1) or tails(2): "))
+
+        ran = random.randint(1,2)
+
+        if ran == 1:
+            print("heads")
+        elif ran == 2:
+               print ("tails")
+
+        if choose == ran:
+            print("you win")
+        elif choose != ran:
+              print ("you lose")
+        else:
+              print("no option found it")
+
+
+
+
+if __name__== "__main__":
+    main()
+
