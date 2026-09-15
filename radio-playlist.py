40b3d74959485d0f4becaac7fc3ca99bba843a96
vladicastillog-dot <vladicastillog@gmail.com>
2026-09-15 15:50:27 +0000

    radiuo

---
+++ radio-playlist.py
@@ -0,0 +1,35 @@
+import time
+
+def main ():
+    playlist = ["Boston", "Dracula", "I Knew It, I Knew You", "hate that i made you love me","Risk It All"]
+    playlist.append("Be By You")
+
+    playlist.insert(0,"Bohemian Rhapsody")
+
+    playlist.pop(4)
+
+    print(playlist)
+    print(playlist.index("Risk It All"))
+    print("Numbers of songs in playlist:", len(playlist))
+    playlist.reverse()
+    print(playlist)
+    playlist.sort()
+    print(playlist)
+
+    #challenge
+
+    repeat = len(playlist)
+    while repeat > 0:
+        print(playlist)
+        song= playlist[0]
+        playlist.pop[0]
+        playlist.append(song)
+        repeat -= 1
+        time.sleep(3)
+
+
+
+
+
+if __name__== "__main__":
+    main()
