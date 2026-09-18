aaeb7d1b0ff7398aae6686bdadf431d9bde931e8
vladicastillog-dot <vladicastillog@gmail.com>
2026-09-18 20:17:55 +0000

    faith

---
+++ for-intro.py
@@ -0,0 +1,11 @@
+def main ():
+
+    doctrine = ["Faith in Jesus","Repentance","Babtism by water","Confirmation","Endure to the end"]
+
+    num = 1
+    for i in doctrine:
+        print(f"{num}. {i}")
+        num += 1
+
+if __name__== "__main__":
+    main()
