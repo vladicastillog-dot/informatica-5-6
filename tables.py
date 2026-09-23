36ef66c1a475d770b57a274cc839c0748e4d1ae1
vladicastillog-dot <vladicastillog@gmail.com>
2026-09-23 17:28:57 +0000

    kinsh

---
+++ tables.py
@@ -0,0 +1,18 @@
+def main ():
+    tabless = [1,2,3,4,5,6,7,8,9,10]
+
+    while true
+        table = input("Enter a number (1-10)")
+        if table == "1":
+            for _ in tables:
+                result = _ *1
+                print(f"{_} times {table} is {result}")
+
+
+
+
+
+
+
+if __name__== "__main__":
+    main()
