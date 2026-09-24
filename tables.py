3ff92a8da77f56af1e654dadc1ee59eb031ca8de
vladicastillog-dot <vladicastillog@gmail.com>
2026-09-24 14:46:12 +0000

    table

--- tables.py
+++ tables.py
@@ -1,12 +1,12 @@
 def main ():
-    tabless = [1,2,3,4,5,6,7,8,9,10]
+    table = input("enter a number 1-10: ")
+    while table != "exit":
+        number = int(table)
+        for x in range(1,11):
+            print(f"{x} times {number} is {x*number}")
+        table = input("enter a number 1-10")
 
-    while true
-        table = input("Enter a number (1-10)")
-        if table == "1":
-            for _ in tables:
-                result = _ *1
-                print(f"{_} times {table} is {result}")
+
 
 
 
